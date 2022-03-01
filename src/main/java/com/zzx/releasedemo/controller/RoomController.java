9157fc7a14d40b723c1acdc4303a4a5f2b11be69
zzx <zzxi1814@163.com>
2022-03-01 15:41:38 +0800

    控制器及pom修改

--- src/main/java/com/zzx/releasedemo/controller/RoomController.java
+++ src/main/java/com/zzx/releasedemo/controller/RoomController.java
@@ -1,6 +1,8 @@
 package com.zzx.releasedemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * description: RoomController
@@ -8,6 +10,12 @@ import org.springframework.stereotype.Controller;
  * author: zzx
  */
 @Controller
+@RequestMapping("/Room")
 public class RoomController {
 
+
+    @GetMapping("/hello")
+    public String test(){
+        return "helloWorld";
+    }
 }
