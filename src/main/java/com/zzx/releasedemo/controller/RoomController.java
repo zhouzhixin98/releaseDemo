aafbf402a3dd6d438883fcac09ed71137fa03102
343766265@qq.com <zzxi1814@163.com>
2022-03-01 16:56:18 +0800

    pom修改

--- src/main/java/com/zzx/releasedemo/controller/RoomController.java
+++ src/main/java/com/zzx/releasedemo/controller/RoomController.java
@@ -3,6 +3,7 @@ package com.zzx.releasedemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * description: RoomController
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RoomController {
 
 
+    @ResponseBody
     @GetMapping("/hello")
     public String test(){
-        return "helloWorld";
+        String result = "helloWorld";
+        return result;
     }
 }
